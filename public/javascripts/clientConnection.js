aa648ee51cec64a7a15ebc07e84027f7e787ecfb
debjyoti88 <debjyoti.88@gmail.com>
2020-06-03 19:46:34 +0800

    socket.io added

---
+++ public/javascripts/clientConnection.js
@@ -0,0 +1,8 @@
+var socket = io('http://192.168.0.102:3000')
+
+// socket.on('connection', function() {
+//     console.log('connection established')
+// })
+// socket.on('message', function(message) {
+//     console.log(message)
+// })
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,33 +7,3 @@ let transcript = document.getElementById('transcript')
 
 /* configure tts */
 tts.setup()
-
-mic_switch.addEventListener('click', function (e) {
-    console.log('mic_switch state', mic_switch.checked)
-    if (mic_switch.checked) {
-        console.log('Mic On.');
-        recognition.start();
-    }
-    else {
-        console.log('Mic Off.');
-        transcript.innerHTML = null;
-        recognition.stop();
-    }
-})
-
-// error button handlers
-prompt1.addEventListener('click', function (e) {
-    tts.speak(prompt1.innerHTML)
-})
-
-prompt2.addEventListener('click', function (e) {
-    tts.speak(prompt2.innerHTML)
-})
-
-prompt3.addEventListener('click', function (e) {
-    tts.speak(prompt3.innerHTML)
-})
-
-prompt4.addEventListener('click', function (e) {
-    tts.speak(prompt4.innerHTML)
-})
